2ffb40dc6b6a9a94fae78238a09fe1ee158f5ecf
Ken Morishita <k_morishita@yumemi.co.jp>
2014-09-06 12:20:48 +0900

    init

---
+++ Assets/Scripts/DebugDisplay.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DebugDisplay : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		string msg = "";
+		if (SmartPhoneInput.IsConnected()) {
+			msg += "Connected, ";
+			msg += "(" + SmartPhoneInput.PadX + "," + SmartPhoneInput.PadY + ")";
+			msg += "(" + SmartPhoneInput.PadGradX + "," + SmartPhoneInput.PadGradY + "," + SmartPhoneInput.PadGradZ + ")";
+		} else {
+			msg += "Disconnected: Please Access " + SmartPhoneController.WebUrl;
+		}
+		guiText.text = msg;
+	}
+}
